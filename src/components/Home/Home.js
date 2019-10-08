f2eef012b1bb1e6978fa15a4060be31a7c64e2da
kylewetton <kylewetton@me.com>
2019-10-08 18:23:03 +1300

    Tweaks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Intro from './Intro';
 import HomeSeries from './HomeSeries';
+import HomeProducts from './HomeProducts.js';
 
 const Home = () => {
     return (
         <React.Fragment>
             <Intro />
             <HomeSeries />
+            <HomeProducts />
         </React.Fragment>
     )
 }
---
+++ src/components/Home/HomeProducts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomeProducts = () => {
+    return <p>Home</p>
+}
+
+export default HomeProducts;
