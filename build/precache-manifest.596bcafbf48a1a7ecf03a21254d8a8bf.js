2dbcb0198397e6654ea439fd94a3db69a3d01855
kylewetton <kylewetton@me.com>
2019-10-09 18:29:29 +1300

    Fix gitignore issue

---
+++ build/precache-manifest.596bcafbf48a1a7ecf03a21254d8a8bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2486022b7ce7aa51a9d2a03bbe9daf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1863c0a4fd83f720132",
+    "url": "/static/css/main.6a0996e8.chunk.css"
+  },
+  {
+    "revision": "5b0627471c31737c1c78",
+    "url": "/static/js/2.ebf797cf.chunk.js"
+  },
+  {
+    "revision": "a1863c0a4fd83f720132",
+    "url": "/static/js/main.55f685e5.chunk.js"
+  },
+  {
+    "revision": "1dbbc9940f80fbac1549",
+    "url": "/static/js/runtime-main.570b0063.js"
+  }
+]);
